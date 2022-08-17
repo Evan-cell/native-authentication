bab51620af36301fa67074e03f195d3be469b6af
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-17 12:12:39 +0300

    button and background image

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { Text, View,Dimensions } from 'react-native';
 import styles from './Styles';
 import Svg, {Image} from 'react-native-svg'
 export default function App() {
+  const {height, width} = Dimensions.get('window')
   return (
     <View style={styles.container}>
-      <Svg>
-        <Image />
+      <Svg height={height} width={width}>
+        <Image href={require('./assets/login-background.jpg')}
+         width={width} 
+         height={height}
+         preserveAspectRatio="xMidyMid slice"
+         />
       </Svg>
+      <View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
