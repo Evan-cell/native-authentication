9d1f31bb863960146f9569af5dbc0e7b85117340
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-17 12:24:56 +0300

    Stylesheet absoluteFill

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View,Dimensions } from 'react-native';
+import { Text, View,Dimensions,StyleSheet } from 'react-native';
 import styles from './Styles';
 import Svg, {Image} from 'react-native-svg'
 export default function App() {
   const {height, width} = Dimensions.get('window')
   return (
     <View style={styles.container}>
+      <View style={StyleSheet.absoluteFill}>
       <Svg height={height} width={width}>
         <Image href={require('./assets/login-background.jpg')}
          width={width} 
@@ -13,9 +14,13 @@ export default function App() {
          preserveAspectRatio="xMidyMid slice"
          />
       </Svg>
+      </View>
       <View>
         <View style={styles.button}>
           <Text style={styles.buttonText}>Log in</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
         </View>
       </View>
       <StatusBar style="auto" />
