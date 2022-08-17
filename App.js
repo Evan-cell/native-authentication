0d746ff9d29a7ecdd3c53c5c66902fcb438710f3
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-17 12:02:19 +0300

    dimensions

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View,Dimensions } from 'react-native';
+import styles from './Styles';
+import Svg, {Image} from 'react-native-svg'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Svg>
+        <Image />
+      </Svg>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
